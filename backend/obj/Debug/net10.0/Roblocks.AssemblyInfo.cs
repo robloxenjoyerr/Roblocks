2f49864529fc4bdac86f5c34cebe167786868fac
“Nikola <Nikola.Spasic@de.bosch.com>
2026-04-27 22:30:51 +0200

    init: changed name to backend

--- backend/obj/Debug/net10.0/Roblocks.AssemblyInfo.cs
+++ backend/obj/Debug/net10.0/Roblocks.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Roblocks")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1d458bce39612ef4803b83a8eae98f9cd987f467")]
 [assembly: System.Reflection.AssemblyProductAttribute("Roblocks")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Roblocks")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
