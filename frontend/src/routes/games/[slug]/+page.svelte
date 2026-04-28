1fb11640ad0740487ac37c20429f0e24b87a9e36
Meister Sebastian (HC/HRL-YT4) <mee3we@bosch.com>
2026-04-28 16:03:26 +0200

    minor changes

---
+++ frontend/src/routes/games/[slug]/+page.svelte
@@ -0,0 +1,13 @@
+<script>
+	import TitleDisplay from './titleDisplay.svelte';
+
+	let { data } = $props();
+</script>
+
+<div class="h-screen w-screen p-10 pl-30">
+	<div class="flex">
+		<img src={data.imageUrl} alt={data.imageDescription} class="h-[50vh]" />
+		<TitleDisplay gameTitle={data.name} creator={data.creator} />
+	</div>
+	<div>{data.description}</div>
+</div>
---
+++ frontend/src/routes/games/[slug]/titleDisplay.svelte
@@ -0,0 +1,10 @@
+<script>
+	let { gameTitle, creator } = $props();
+</script>
+
+<div class=" flex-col justify-center pt-6 pl-6">
+	<h1 class="text-4xl font-bold">{gameTitle}</h1>
+	<div>By <span class="font-bold">{creator}</span></div>
+	<div class="items-stretch"></div>
+	<button class="h-13 w-full rounded-xl bg-green-600">Play</button>
+</div>
